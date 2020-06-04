fb2749371455f1576702d9b6e8db21979a16a93d
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-04 19:54:18 +0300

    add middleware for authentication

--- src/account/routes.rs
+++ src/account/routes.rs
@@ -1,6 +1,12 @@
 use actix_web::web;
 
+use crate::auth::middleware::RequireAuthentication;
+
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.route("", web::post().to(super::handler::create_account))
-        .route("/{id}", web::get().to(super::handler::get_account));
+        .service(
+            web::resource("/{id}")
+                .wrap(RequireAuthentication)
+                .route(web::get().to(super::handler::get_account)),
+        );
 }
--- src/auth/middleware.rs
+++ src/auth/middleware.rs
@@ -1,4 +1,17 @@
-use actix_identity::{CookieIdentityPolicy, IdentityService};
+use actix_identity::{CookieIdentityPolicy, IdentityService, RequestIdentity};
+use actix_service::{Service, Transform};
+use actix_web::{
+    dev::{ServiceRequest, ServiceResponse},
+    Error, HttpResponse,
+};
+use futures::{
+    future::{ok, Ready},
+    Future,
+};
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use crate::auth::decode_jwt;
 
 pub fn default() -> IdentityService<CookieIdentityPolicy> {
     IdentityService::new(
@@ -8,4 +21,73 @@ pub fn default() -> IdentityService<CookieIdentityPolicy> {
         .name("authorization")
         .secure(false),
     )
+}
+
+pub struct RequireAuthentication;
+
+impl<S, B> Transform<S> for RequireAuthentication
+where
+    S: Service<
+        Request = ServiceRequest,
+        Response = ServiceResponse<B>,
+        Error = Error,
+    >,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = RequireAuthenticationMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(RequireAuthenticationMiddleware { service })
+    }
+}
+
+pub struct RequireAuthenticationMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service for RequireAuthenticationMiddleware<S>
+where
+    S: Service<
+        Request = ServiceRequest,
+        Response = ServiceResponse<B>,
+        Error = Error,
+    >,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: ServiceRequest) -> Self::Future {
+        let auth_token = req.get_identity().unwrap_or("".to_string());
+        if let Err(_) = decode_jwt(&auth_token) {
+            return Box::pin(async move {
+                Ok(req.into_response(
+                    HttpResponse::Unauthorized().finish().into_body(),
+                ))
+            });
+        };
+
+        let fut = self.service.call(req);
+        Box::pin(async move {
+            let res = fut.await?;
+            Ok(res)
+        })
+    }
 }
