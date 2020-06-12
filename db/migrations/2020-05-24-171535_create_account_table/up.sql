fb4d208801e08222615750acb9780ebb2a41cb90
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-12 18:57:18 +0300

    scripts should be mounted

--- db/migrations/2020-05-24-171535_create_account_table/up.sql
+++ db/migrations/2020-05-24-171535_create_account_table/up.sql
@@ -4,9 +4,11 @@ CREATE TABLE account (
   updated_at TIMESTAMP,
   first_name VARCHAR(64),
   last_name VARCHAR(64),
-  email VARCHAR(256) NOT NULL UNIQUE,
+  email VARCHAR(256) NOT NULL,
   password BYTEA NOT NULL
 );
+
+CREATE UNIQUE INDEX key_email ON account (LOWER(email));
 
 GRANT USAGE, SELECT ON SEQUENCE account_id_seq TO mainman_client;
 GRANT SELECT, INSERT, UPDATE ON TABLE account TO mainman_client;
