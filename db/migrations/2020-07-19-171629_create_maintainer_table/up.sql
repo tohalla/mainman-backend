af62701cccf3b4de714f37975b8e8cee1f0485c4
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2020-07-19 20:23:49 +0300

    add maintainer table

---
+++ db/migrations/2020-07-19-171629_create_maintainer_table/up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE maintainer (
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP,
+  organisation INTEGER NOT NULL REFERENCES organisation (id) ON DELETE CASCADE,
+  account INTEGER NOT NULL REFERENCES account (id),
+  details JSONB
+);
+
+GRANT USAGE, SELECT ON SEQUENCE maintainer_id_seq TO mainman_client;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE maintainer TO mainman_client;
