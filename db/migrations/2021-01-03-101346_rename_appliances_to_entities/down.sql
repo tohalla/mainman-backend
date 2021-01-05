95fb40a0101ec9180d1248f3e2be6913c5dac38a
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-05 16:17:43 +0200

    fix: should also rename constraints

--- db/migrations/2021-01-03-101346_rename_appliances_to_entities/down.sql
+++ db/migrations/2021-01-03-101346_rename_appliances_to_entities/down.sql
@@ -1,3 +1,10 @@
+ALTER TABLE entity RENAME CONSTRAINT entity_organisation_fkey TO appliance_organisation_fkey;
+ALTER TABLE maintenance_event RENAME CONSTRAINT maintenance_event_entity_fkey TO maintenance_event_appliance_fkey;
+ALTER TABLE maintainer_entity RENAME CONSTRAINT maintainer_entity_entity_fkey TO maintainer_appliance_appliance_fkey;
+ALTER TABLE maintainer_entity RENAME CONSTRAINT maintainer_entity_maintainer_fkey TO maintainer_appliance_maintainer_fkey;
+ALTER TABLE entity RENAME CONSTRAINT entity_pkey TO appliance_pkey;
+ALTER TABLE maintainer_entity RENAME CONSTRAINT maintainer_entity_pkey TO maintainer_appliance_pkey;
+
 ALTER TABLE maintenance_event RENAME COLUMN entity TO appliance;
 
 ALTER TABLE maintainer_entity RENAME TO maintainer_appliance;
--- db/migrations/2021-01-03-101346_rename_appliances_to_entities/up.sql
+++ db/migrations/2021-01-03-101346_rename_appliances_to_entities/up.sql
@@ -4,3 +4,10 @@ ALTER TABLE maintainer_appliance RENAME TO maintainer_entity;
 ALTER TABLE maintainer_entity RENAME COLUMN appliance TO entity;
 
 ALTER TABLE maintenance_event RENAME COLUMN appliance TO entity;
+
+ALTER TABLE entity RENAME CONSTRAINT appliance_organisation_fkey TO entity_organisation_fkey;
+ALTER TABLE maintenance_event RENAME CONSTRAINT maintenance_event_appliance_fkey TO maintenance_event_entity_fkey;
+ALTER TABLE maintainer_entity RENAME CONSTRAINT maintainer_appliance_appliance_fkey TO maintainer_entity_entity_fkey;
+ALTER TABLE maintainer_entity RENAME CONSTRAINT maintainer_appliance_maintainer_fkey TO maintainer_entity_maintainer_fkey;
+ALTER TABLE entity RENAME CONSTRAINT appliance_pkey TO entity_pkey;
+ALTER TABLE maintainer_entity RENAME CONSTRAINT maintainer_appliance_pkey TO maintainer_entity_pkey;
