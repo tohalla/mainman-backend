0a26c6057ba1be5c5fd96e9a10ccb9cd05deef61
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-05 11:42:30 +0200

    fix: auth routes behind middleware

--- src/auth/routes.rs
+++ src/auth/routes.rs
@@ -1,7 +1,12 @@
 use actix_web::web;
 
+use super::middleware::RequireAuthentication;
+
 pub fn routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(super::handler::get_account)
-        .service(super::handler::authenticate)
-        .service(super::handler::sign_out);
+    cfg.service(super::handler::authenticate).service(
+        web::scope("")
+            .wrap(RequireAuthentication::default())
+            .service(super::handler::get_account)
+            .service(super::handler::sign_out),
+    );
 }
