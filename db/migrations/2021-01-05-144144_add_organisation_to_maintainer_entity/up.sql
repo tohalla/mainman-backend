e8bf1469e98e6ff801317bff7aef49d1b8ef9f0f
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-07 23:07:40 +0200

    fixup! fix: organisation access check

--- db/migrations/2021-01-05-144144_add_organisation_to_maintainer_entity/up.sql
+++ db/migrations/2021-01-05-144144_add_organisation_to_maintainer_entity/up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE entity ADD CONSTRAINT entity_organisation_key UNIQUE (hash, organisation);
 ALTER TABLE maintainer ADD CONSTRAINT maintainer_organisation_key UNIQUE (id, organisation);
-ALTER TABLE maintainer_entity ADD COLUMN organisation integer,
+ALTER TABLE maintainer_entity ADD COLUMN organisation integer NOT NULL,
   DROP CONSTRAINT maintainer_entity_entity_fkey,
   DROP CONSTRAINT maintainer_entity_maintainer_fkey,
   ADD CONSTRAINT maintainer_entity_entity_fkey FOREIGN KEY (entity, organisation) REFERENCES entity (hash, organisation),
