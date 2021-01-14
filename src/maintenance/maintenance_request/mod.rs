bd27084d802437180ccd08c9aa8294236eed3d75
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-14 17:29:17 +0200

    feature: methods for MaintenanceRequest

--- src/maintenance/maintenance_request/mod.rs
+++ src/maintenance/maintenance_request/mod.rs
@@ -1,7 +1,13 @@
 use chrono::NaiveDateTime;
+use diesel::prelude::*;
 use uuid::Uuid;
 
-use crate::schema::maintenance_request;
+use crate::{
+    db::{Connection, Creatable},
+    error::Error,
+    schema::maintenance_request,
+    MainmanResult,
+};
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Identifiable)]
 #[table_name = "maintenance_request"]
@@ -11,4 +17,39 @@ pub struct MaintenanceRequest {
     pub updated_by: Option<i32>,
     pub entity: Uuid,
     pub description: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "maintenance_request"]
+pub struct NewMaintenanceRequest {
+    entity: Uuid,
+    description: String,
+}
+
+impl MaintenanceRequest {
+    pub fn get(
+        id: i64,
+        conn: &Connection,
+    ) -> MainmanResult<MaintenanceRequest> {
+        Ok(maintenance_request::table.find(id).first::<Self>(conn)?)
+    }
+
+    pub fn by_entity(
+        entity: Uuid,
+        conn: &Connection,
+    ) -> MainmanResult<Vec<Self>> {
+        use crate::schema::maintenance_request::dsl;
+        Ok(dsl::maintenance_request
+            .filter(dsl::entity.eq(entity))
+            .load::<MaintenanceRequest>(conn)
+            .map_err(|_| Error::NotFoundError)?)
+    }
+}
+
+impl Creatable<MaintenanceRequest> for NewMaintenanceRequest {
+    fn create(&self, conn: &Connection) -> MainmanResult<MaintenanceRequest> {
+        Ok(diesel::insert_into(maintenance_request::table)
+            .values(self)
+            .get_result::<MaintenanceRequest>(conn)?)
+    }
 }
