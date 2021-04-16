d01b8a08ab125d7d9f0b1c092d8f37cb89a82cda
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-04-16 23:42:28 +0300

    fixup! feat: no duplicate events for maintenance requests

--- db/migrations/2021-02-06-123913_alter_stripe_price_to_jsonb/down.sql
+++ db/migrations/2021-02-06-123913_alter_stripe_price_to_jsonb/down.sql
@@ -1,1 +1,13 @@
 ALTER TABLE plan ALTER COLUMN stripe_price TYPE TEXT USING null;
+
+CREATE OR REPLACE VIEW entity_overview as
+  SELECT
+    entity.uuid,
+    entity.organisation,
+    COUNT(DISTINCT maintenance_request.id) FILTER (WHERE maintenance_event.maintenance_request IS NULL) pending_requests,
+    COUNT(DISTINCT maintenance_request.id) FILTER (WHERE maintenance_event.maintenance_request IS NOT NULL AND maintenance_event.resolved_at IS NULL) unfinished_requests,
+    COUNT(DISTINCT maintenance_request.id) FILTER (WHERE maintenance_event.resolved_at IS NOT NULL) finished_requests
+  FROM entity
+    LEFT JOIN maintenance_request ON maintenance_request.entity = entity.uuid
+    LEFT JOIN maintenance_event ON maintenance_event.maintenance_request = maintenance_request.id
+  GROUP BY entity.uuid, entity.organisation;
--- db/migrations/2021-04-16-153054_add_processed_at_to_maintenance_request/up.sql
+++ db/migrations/2021-04-16-153054_add_processed_at_to_maintenance_request/up.sql
@@ -1,1 +1,15 @@
 ALTER TABLE maintenance_request ADD COLUMN processed_at timestamp;
+
+CREATE OR REPLACE VIEW entity_overview as
+  SELECT
+    entity.uuid,
+    entity.organisation,
+    COUNT(DISTINCT maintenance_request.id) FILTER (WHERE maintenance_request.processed_at IS NULL) pending_requests,
+    COUNT(DISTINCT maintenance_request.id) FILTER (WHERE maintenance_event.maintenance_request IS NOT NULL AND maintenance_event.resolved_at IS NULL) unfinished_requests,
+    COUNT(DISTINCT maintenance_request.id) FILTER (WHERE maintenance_event.resolved_at IS NOT NULL) finished_requests
+  FROM entity
+    LEFT JOIN maintenance_request ON maintenance_request.entity = entity.uuid
+    LEFT JOIN maintenance_event ON maintenance_event.maintenance_request = maintenance_request.id
+  GROUP BY entity.uuid, entity.organisation;
+
+GRANT SELECT ON entity_overview TO mainman_client;
